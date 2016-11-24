f8046e6dcb6d29640b040d47aba21ac3aa2f5dca
wangyahan <wangyahan@hotmail.com>
2016-11-24 17:05:12 +0800

    首次提交到本地仓库

---
+++ git_demo/src/git_demo/mytest.java
@@ -0,0 +1,15 @@
+package git_demo;
+
+
+public class mytest {
+	
+	public static void main(String[] args) {  
+     
+        try {  
+            System.out.println("git_demo≤‚ ‘");  
+        } catch (Exception e) {  
+            e.printStackTrace();  
+        }  
+    }  
+
+}
