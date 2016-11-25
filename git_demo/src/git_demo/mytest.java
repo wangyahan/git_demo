5840b846591ac6daf0ae860956e4ae1dd6e39689
wangyahan <wangyahan@hotmail.com>
2016-11-25 09:35:52 +0800

    修改

--- git_demo/src/git_demo/mytest.java
+++ git_demo/src/git_demo/mytest.java
@@ -6,7 +6,8 @@ public class mytest {
 	public static void main(String[] args) {  
      
         try {  
-            System.out.println("git_demo≤‚ ‘");  
+            System.out.println("git_demo≤‚ ‘");
+            System.out.println("git_demo≤‚ ‘123");
         } catch (Exception e) {  
             e.printStackTrace();  
         }  
